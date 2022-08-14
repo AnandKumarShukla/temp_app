b6e1b6218ecc26b968a90274e3e70cb57a21bbcc
anandfzd668 <anandfzd668@gmail.com>
2022-08-14 12:05:11 +0530

    added New_changes

--- app.js
+++ app.js
@@ -1,1 +1,2 @@
 console.log('Welcome to to git projects..');
+console.log('Git change')
--- git/welcome.js
+++ git/welcome.js
@@ -2,6 +2,3 @@ console.log("Hello Git ");
 console.log('This is second changes');
 console.log('This is second changes');
 console.log('This is second changes');
-console.log('This is second changes');
-console.log('This is second changes');
-console.log('This is second changes');
